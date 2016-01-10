aa43581c698399a66c16fa0c35671687e9b65d43
TakeumiYamamura <yama1223xxx@gmail.com>
2016-01-10 12:43:20 +0900

    create script for making latex table

---
+++ plot/make_latex_table.r
@@ -0,0 +1,14 @@
+library(Hmisc)
+
+# サンプルデータを作成
+data <- matrix(1:20, ncol = 5, nrow = 4)
+colnames(data) <- c("DE", "DE\nalpha=(0.5)", "DE\nalpha=(1.0)", "DE\nalpha=(1.5)", "DE\nalpha=(2.0)")
+rownames(data) <- c("F1", "F2", "F3", "F4","F5","F6", "F7", "F8", "F9","F10","F11", "F12", "F13", "F14","F15")
+
+latex(
+  data,                        # 配列型のデータ
+  file="latex_table.tex",                     # LaTeX ファイルの保存先
+  title="F",              # 1 行 1 列目のセルの内容
+  label="ref-tb-values",       # LaTeX の \label に相当
+  caption="提案手法と通常のDEの比較実験"  # LaTeX の \caption に相当
+)
